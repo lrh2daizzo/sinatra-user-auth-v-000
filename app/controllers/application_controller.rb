cd27d26b78c1a75491e5fd346ee818c60db95a22
Luis Herrera <herrera.lrh@gmail.com>
2020-11-22 01:53:30 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -16,6 +16,10 @@ class ApplicationController < Sinatra::Base
   end
 
   post '/registrations' do
+    @user = User.new(name: params[:name], email: params[:email], password: params[:password])
+    @user.save
+    session[:user_id] = @user.id 
+    
     erb :'/users/home'
   end
 end
